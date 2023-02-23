8680e0ce1c5326491587401d0e89a5192f8ae9ee
fatihisla <116791165+fatihisla@users.noreply.github.com>
2023-02-23 09:26:24 +0100

    Prima Versione

---
+++ islamovski.fatih.4H.ReadCSV/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using islamovski.fatih._4H.ReadCSV.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace islamovski.fatih._4H.ReadCSV
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                dgDati.ItemsSource = new Utenti("utente.csv");
+            }
+            catch (Exception errore)
+            {
+                MessageBox.Show(errore.Message);
+            }
+
+            private void gdDati_Loading(object sender, DataGridRowEventArgs e)
+            {
+                try
+                {
+                    if (e != null && e.Row != null)
+                    {
+
+                        Utente u = e.Row.Item as Utente;
+                        if (u != null)
+                            if (u.Tipo == "docente")
+                                e.Row.Background = Brushes.Green;
+                        if (u.Tipo == "studente")
+                            e.Row.Background = Brushes.Blue;
+                    }
+                }
+                catch { };
+            }
+        }
+    }
+}
---
+++ islamovski.fatih.4H.ReadCSV/Models/Utente.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static islamovski.fatih._4H.ReadCSV.Models.Utente;
+
+namespace islamovski.fatih._4H.ReadCSV.Models
+{
+    public class Utente
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Email { get; set; }
+        public string Tipo { get; set; }
+        public Utente() 
+        { 
+            
+        } 
+        public Utente(string str)
+        {
+
+            string[] colonne = str.Split(';');
+
+            Nome = colonne[0];
+            Cognome = colonne[1];
+            Email = colonne[2];
+            Tipo = colonne[3];
+        }
+    }
+        public class Utenti : List<Utente>
+        {
+            public Utenti(string filename)
+            {
+                StreamReader fin = new StreamReader("Utenti.csv");
+                fin.ReadLine();
+                while (!fin.EndOfStream)
+                {
+                    string str = fin.ReadLine();
+                    Add(new Utente(str));
+                }
+                fin.Close();
+            }
+
+        }
+}
+
+        
+       
+    
+
